9b1f9fad7422662a183f0abe5deedb3a66ecf064
acerise <42525915+acerise85@users.noreply.github.com>
2018-12-11 18:35:03 -0700

    Giphs now showing on screen with on click of button, it is showing all topics though instead of just one

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,25 +1,39 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    var topics = ['Gentle Giants', 'Floofy Doggos', 'Silent Hunters'];
-
-   
-
-    for (i = 0; i < topics.length; i++){
-
+    var topics = ['Dog', 'Cat', 'Elephant'];
+    $("#button").empty();
+    for (i = 0; i < topics.length; i++) {
+        
         var gifButton = $("<button>");
 
-       
         gifButton.text(topics[i]);
         $("#button").append(gifButton);
     }
+    $('#button').on('click', function () {
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + elephant + "&api_keyFiNQYJ3gkPlRMs1mDNZ93hgjtmVyZbDYlimit=10"
-    
-    $.ajax({
-       url: queryURL,
-       method: "GET"
-    }).then(function(response){
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=FiNQYJ3gkPlRMs1mDNZ93hgjtmVyZbDY&limit=10";
+        
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
 
-        console.log(response);
+            console.log(response);
+       
+            var results = response.data;
+
+            for (i = 0; i < results.length; i++){
+
+                var topicDiv = $('<div>');
+                var s = $('<span>');
+                var topicPic = $('<img>');
+
+                topicPic.attr("src", results[i].images.fixed_height.url);
+                $(topicDiv).append(s,topicPic );
+                $("#giphy-pics").append(topicDiv);
+            }
+       
+       
+        })
     })
 })
