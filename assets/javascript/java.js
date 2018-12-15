ff63b8103fc18c2a38949758f35bc6460e826a6d
acerise <42525915+acerise85@users.noreply.github.com>
2018-12-15 08:12:13 -0700

    buttons correctly pulling images

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
     var topics = ['Dog', 'Cat', 'Elephant'];
 
-    $("#gifButton").empty(); 
+    // $("#gifButton").empty(); 
 
     //For loop to make button for each item in the topics array
     for (i = 0; i < topics.length; i++) {
@@ -11,32 +11,37 @@ $(document).ready(function () {
         gifButton.attr("data-topic", topics[i]);
         gifButton.text(topics[i]);
         $("#button-div").append(gifButton);
+        console.log(gifButton);
     }
     //On click event for buttons 
-    $('#gifButton').on('click', function () {
-        event.preventDefault();
-        var picture = $(this).attr("data-topic");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + picture + "&api_key=FiNQYJ3gkPlRMs1mDNZ93hgjtmVyZbDY&limit=10";
-        //giphy API URL call
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
 
+    $('button').on('click', function () {
+    // event.preventDefault();
+    var picture = $(this).attr("data-topic");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + picture + "&api_key=FiNQYJ3gkPlRMs1mDNZ93hgjtmVyZbDY&limit=10";
+    //giphy API URL call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(queryURL);
+            console.log(response);
             var results = response.data;
+           
+            for (i = 0; i < results.length; i++) {
 
-    for (i = 0; i < results.length; i++) {
+                var giphyPics = $('<div>');
+                var p = $('<p>');
+                var topicPic = $('<img>');
+                topicPic.attr("src", results[i].images.fixed_height.url);
+                $(giphyPics).append(p, topicPic);
+                $("#giphyPics").append(topicPic);
 
-        var giphyPics = $('<div>');
-        var p = $('<p>');
-        var topicPic = $('<img>');
-        topicPic.attr("src", results[i].images.fixed_height.url);
-        $(giphyPics).append(p, picture);
-        $("#giphyPics").append(topicPics);
-        console.log(results);
+                // console.log(button)
+                
 
-    }
+            }
         })
     })
-    
+
 })
